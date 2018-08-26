9e2aa5129fbc58e8cdf284c7871286d831022971
alex <alexander.vaagan@gmail.com>
2018-08-26 23:33:07 +0200

    stuff

--- StockBot.py
+++ StockBot.py
@@ -1,86 +1,161 @@
+#https://medium.com/@ts1829/policy-gradient-reinforcement-learning-in-pytorch-df1383ea0baf
+
+import gym
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+from tqdm import tqdm, trange
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.distributions import Categorical
 
+# env = gym.make('CartPole-v1')
 from game import Game
-import torch
-from torch import nn, optim
-from torch.autograd import Variable
-import torch.nn.functional as F
-
-random.seed(111)
-
-
-
-#
-# data = generate_data()
-# data_pct = generate_features(data)
-
-
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv1d(1, 6, 5)
-        self.conv2 = nn.Conv1d(6, 16, 5)
-        self.fc1 = nn.Linear(16*22, 3)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(-1, 16 * 22)
-        x = F.sigmoid(self.fc1(x))
-        return x
-
-net = Net()
-print(net)
 
 env = Game()
-ob = env.reset()
+# env.seed(1)
+torch.manual_seed(1)
 
-obs, acs, rews = [], [], []
-done = False
-while not done:
-
-    x = torch.from_numpy(ob).float()
-    with torch.no_grad():
-        obs.append(ob)
-        probs = net.forward(x)
-        m = Categorical(probs)
-        ac = m.sample()
-
-    acs.append(ac.numpy())
-    ob, rew, done = env.step(ob, ac.numpy())
-    loss = -m.log_prob(ac) * torch.from_numpy(rew).float()
-    rews.append(rew)
-
-# Compute Q-Values
-gamma = 1
-q_n = []
-reward_to_go = False
-for path in rews:
-    q = 0
-    q_path = []
-
-    # Dynamic programming over reversed path
-    for rew in reversed(path):
-        q = rew + gamma * q
-        q_path.append(q)
-    q_path.reverse()
-
-    # Append these q values
-    if not reward_to_go:
-        q_path = [q_path[0]] * len(q_path)
-    q_n.extend(q_path)
+# Hyperparameters#Hyperp
+learning_rate = 0.01
+gamma = 0.99
 
 
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.state_space = env.observation_space
+        self.action_space = env.action_space.n
 
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+        self.l1 = nn.Linear(self.state_space, 128, bias=False)
+        self.l2 = nn.Linear(128, self.action_space, bias=False)
 
-optimizer.zero_grad()
+        self.gamma = gamma
 
-loss.backward()
-optimizer.step()
+        # Episode policy and reward history
+        # self.policy_history = Variable(torch.Tensor())
+        self.policy_history = []
+        self.reward_episode = []
+        # Overall reward and loss history
+        self.reward_history = []
+        self.loss_history = []
+
+    def forward(self, x):
+        model = torch.nn.Sequential(
+            self.l1,
+            nn.Dropout(p=0.6),
+            nn.ReLU(),
+            self.l2,
+            nn.Softmax(dim=-1)
+        )
+        return model(x)
+
+
+policy = Policy()
+optimizer = optim.Adam(policy.parameters(), lr=learning_rate)
+
+
+def select_action(state):
+    # Select an action (0 or 1) by running policy model and choosing based on the probabilities in state
+    # state = torch.from_numpy(state).type(torch.FloatTensor)
+    state = torch.from_numpy(state).float()
+    state = policy(Variable(state))
+    c = Categorical(state)
+    action = c.sample()
+
+    # Add log probability of our chosen action to our history
+    # if policy.policy_history.dim() != 0:
+    #     policy.policy_history = torch.cat([policy.policy_history, c.log_prob(action)])
+    # else:
+    #     policy.policy_history = (c.log_prob(action))
+    policy.policy_history.append(c.log_prob(action))
+    return action
+
+
+def update_policy():
+    R = 0
+    rewards = []
+
+    # Discount future rewards back to the present using gamma
+    for r in policy.reward_episode[::-1]:
+        R = r + policy.gamma * R
+        rewards.insert(0, R)
+
+    # Scale rewards
+    rewards = torch.FloatTensor(rewards)
+    eps = float(np.finfo(np.float32).eps)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+
+    # Calculate loss
+    history = torch.stack(policy.policy_history, 0)
+    # loss = (torch.sum(torch.mul(policy.policy_history, Variable(rewards)).mul(-1), -1))
+    loss = (torch.sum(torch.mul(history, Variable(rewards)).mul(-1), -1))
+
+    # Update network weights
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Save and intialize episode history counters
+    policy.loss_history.append(loss.data[0])
+    policy.reward_history.append(np.sum(policy.reward_episode))
+    # policy.policy_history = Variable(torch.Tensor())
+    policy.policy_history = []
+    policy.reward_episode = []
+
+
+def main(episodes):
+    running_reward = 10
+    for episode in range(episodes):
+        state = env.reset()  # Reset environment and record the starting state
+        done = False
+
+        for time in range(1000):
+            action = select_action(state)
+            # Step through environment using chosen action
+            # state, reward, done, _ = env.step(action.data[0])
+            state, reward, done, _ = env.step(state, action.numpy())
+
+            # Save reward
+            policy.reward_episode.append(reward)
+            if done:
+                break
+
+        # Used to determine when the environment is solved.
+        running_reward = (running_reward * 0.99) + (time * 0.01)
+
+        update_policy()
+
+        if episode % 50 == 0:
+            print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(episode, time, running_reward))
+            print("running_reward: {}, reward_threshold: {}, solved: {}".format(running_reward, env.spec.reward_threshold, running_reward > env.spec.reward_threshold))
+
+        if running_reward > env.spec.reward_threshold:
+            print("Solved! Running reward is now {} and the last episode runs to {} time steps!".format(running_reward,
+                                                                                                        time))
+            break
+
+
+episodes = 1000
+main(episodes)
+
+window = int(episodes/20)
+
+fig, ((ax1), (ax2)) = plt.subplots(2, 1, sharey=True, figsize=[9,9]);
+rolling_mean = pd.Series(policy.reward_history).rolling(window).mean()
+std = pd.Series(policy.reward_history).rolling(window).std()
+ax1.plot(rolling_mean)
+ax1.fill_between(range(len(policy.reward_history)),rolling_mean-std, rolling_mean+std, color='orange', alpha=0.2)
+ax1.set_title('Episode Length Moving Average ({}-episode window)'.format(window))
+ax1.set_xlabel('Episode'); ax1.set_ylabel('Episode Length')
+
+ax2.plot(policy.reward_history)
+ax2.set_title('Episode Length')
+ax2.set_xlabel('Episode'); ax2.set_ylabel('Episode Length')
+
+fig.tight_layout(pad=2)
+plt.show()
+#fig.savefig('results.png')
--- game.py
+++ game.py
@@ -1,31 +1,28 @@
 
 import numpy as np
 import pandas as pd
+from gym import spaces
+from gym.envs.registration import EnvSpec
 
-nb_sources = 10
 f = 5
 timesteps = 1000
 lookback_window = 30
 
 
 def generate_data():
-    data = []
-    for i in range(nb_sources):
-        phase = np.random.uniform(-0.5, 0.5, 1)
+    phase = np.random.uniform(-0.5, 0.5, 1)
 #         f = np.random.uniform(1, 50, 1)
-        x = np.linspace(0, 1, timesteps) + phase
-        y = np.sin(2*np.pi*x*f) * 1
-        y = y + np.random.uniform(-0.5, 0.5, timesteps).cumsum()
-        data.append(y)
-    data = np.asarray(data)
+    time = np.linspace(0, 1, timesteps) + phase
+    x = np.sin(2*np.pi*time*f) * 1
+    x = x + np.random.uniform(-0.5, 0.5, timesteps).cumsum()
+    data = np.asarray(x)
     return data
 
 
 def generate_features(data):
     df = pd.DataFrame(data)
-    df = df.T
     df_pct = ((df.shift(-1) - df) / df.abs())
-    df_pct = np.asarray([df_pct[i:i + lookback_window].T.values for i in range(0, df_pct.shape[0] - lookback_window, 1)])
+    df_pct = np.asarray([df_pct[i:i + lookback_window].values for i in range(0, df_pct.shape[0] - lookback_window, 1)])
     return df_pct
 
 
@@ -43,87 +40,78 @@ class Game:
     }
 
     def __init__(self):
+        self.observation_space = lookback_window
+        self.action_space = spaces.Discrete(3)
         self.lookback_window = lookback_window
-        self.states = np.full(nb_sources, self.ST_NOT_INVESTED)
-        self.rewards = np.zeros(shape=(nb_sources), dtype=np.float32)
-        self.observations = []
+        # self.states = np.full(nb_sources, self.ST_NOT_INVESTED)
+        self.states = self.ST_NOT_INVESTED
+        self.price = 0
+        # self.rewards = np.zeros(shape=(nb_sources), dtype=np.float32)
+        self.rewards = 0
         pct_data = generate_features(generate_data())
         self.data = pct_data
         self.num_periods = len(self.data)
         # self.data = np.random.uniform(-10, 10, size=[batch_size, num_periods*lookback_window]).cumsum(axis=1).reshape([-1, 1, num_periods*lookback_window])
         self._step = 0
-
+        self.spec = EnvSpec(id='StocBot-v1', reward_threshold=10000)
 
     def reset(self):
-        batch =  self.data[self._step]
-        batch = batch.reshape(nb_sources, 1, lookback_window)
+        pct_data = generate_features(generate_data())
+        self.data = pct_data
+        self._step = 0
+        self.price = 0
+        self.rewards = 0
+        self.states = self.ST_NOT_INVESTED
+
+        batch = self.data[self._step]
+        # batch = batch.reshape(nb_sources, 1, lookback_window)
         self._step += 1
-        return batch
+        return batch.reshape(-1)
 
-
-    def _sell(self, row, _ob):
+    def _sell(self, _ob):
         reward = 0
-        _state = self.states[row]
 
-        if self.ST_INVESTED == _state:
-            reward += (_ob.reshape(self.lookback_window)[-1] - self.observations[row].reshape(self.lookback_window)[-1])
-            self.states[row] = self.ST_NOT_INVESTED
+        if self.ST_INVESTED == self.states:
+            reward += (_ob[-1] - self.price)
+            self.states = self.ST_NOT_INVESTED
+            self.price = 0
         else:
             reward += 0  # One could consider rewarding the fact that this is a correct decision IF we where invested
 
         return reward
 
-    def _invest(self, row, _ob):
+    def _invest(self, _ob):
         reward = 0
-        _state = self.states[row]
 
-        if self.ST_NOT_INVESTED == _state:
+        if self.ST_NOT_INVESTED == self.states:
+            self.states = self.ST_INVESTED
+            self.price = _ob[-1]
             reward += 0  # Because we dont know wether it is a good action or not.
-            self.states[row] = self.ST_INVESTED
 
         return reward
 
-    def _nothing(self, row, _ob):
+    def _nothing(self, _ob):
         # Might give com creds for doing nothing in some circumstances
         reward = 0
         return reward
 
     def step(self, observations, action):
-        rewards = np.zeros(shape=(nb_sources), dtype=np.float32)
-        for row, ob, ac in zip(range(len(observations)), observations, action):
+        reward = 0
+        # rewards = np.zeros(shape=(nb_sources), dtype=np.float32)
+        # for row, ob, ac in zip(range(len(observations)), observations, action):
             # Update states
-            if self.AC_SELL == ac:
-                sell = self._sell(row, ob)
-                rewards[row] = sell
-            elif self.AC_INVEST == ac:
-                rewards[row] = self._invest(row, ob)
-            elif self.AC_NOTHING == ac:
-                rewards[row] = self._nothing(row, ob)
+        if self.AC_INVEST == action:
+            reward = self._invest(observations)
+        elif self.AC_SELL == action:
+            sell = self._sell(observations)
+            reward = sell
+        elif self.AC_NOTHING == action:
+            reward = self._nothing(observations)
 
-            self.observations.append(ob)
+            # self.observations.append(ob)
 
-        batch = self.data[self._step]
-        batch = batch.reshape(nb_sources, 1, lookback_window)
+        batch = self.data[self._step].reshape(-1)
         self._step += 1
-        done = self._step == self.num_periods-1
+        done = self._step == self.num_periods
 
-        return batch, rewards, done
-
-    def play(self, observations, actions):
-        action_ind = np.argmax(actions, axis=1)
-        batch_size, num_periods, _ = observations.shape
-        for si in range(batch_size):
-            ob = observations[si][0][-1]
-            ac = action_ind[si]
-
-            # Update states
-            if self.AC_SELL == ac:
-                self.rewards[si] += self._sell(si, ob)
-            elif self.AC_INVEST == ac:
-                self.rewards[si] += self._invest(si, ob)
-            elif self.AC_NOTHING == ac:
-                self.rewards[si] += self._nothing(si, ob)
-
-            self.observations.append(ob)
-
-        return self.rewards
+        return batch, reward, done, dict()
